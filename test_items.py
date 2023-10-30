050aceced79db7137fddb2a0c3a5af37184e56a1
ValeriiSidorov <139145572+ValeriiSidorov@users.noreply.github.com>
2023-10-31 02:11:55 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 import time
 
 
-def test_different_languages(browser):
+def test_add_to_cart_button(browser):
     browser.get("https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     # time.sleep(30)
     
